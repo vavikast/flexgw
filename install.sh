929b06af3a16fa52b997251c9f561be8a49a1248
vavikast <30380259+vavikast@users.noreply.github.com>
2019-04-18 09:38:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sysctl -a | egrep "ipv4.*(accept|send)_redirects" | awk -F "=" '{print $1 "=0"}'
 cat a.txt >>/etc/sysctl.conf
 
 cat /etc/sysctl.conf | sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
+echo "/net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
 ####----rpm----####
 yum -y install strongswan openvpn zip curl wget
@@ -14,9 +15,9 @@ rpm -ivh flexgw-2.5.0-1.el6.x86_64.rpm
 cp -f /usr/local/flexgw/rc/strongswan.conf /etc/strongswan/strongswan.conf
 cp -f /usr/local/flexgw/rc/openvpn.conf /etc/openvpn/server.conf
 
-cat >> /etc/openvpn/server.conf << EOF
-push "redirect-gateway def1 bypass-dhcp"
-EOF
+echo 'push "redirect-gateway def1 bypass-dhcp"' >>  /etc/openvpn/server.conf
+
+
 
 
 cat /etc/strongswan/strongswan.d/charon/dhcp.conf | sed  -i 's/load = yes/#load = yes/' /etc/strongswan/strongswan.d/charon/dhcp.conf 
